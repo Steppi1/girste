5eaf23fd6d7e434f6576ac4b41f7062e8b1e0a67
Steepee <114439934+Steppi1@users.noreply.github.com>
2025-05-13 14:06:43 +0200

    gg

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const SUPABASE_URL = 'https://mcvvvhpmpouuupwqlbsn.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jdnZ2aHBtcG91dXVwd3FsYnNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5ODY5NzEsImV4cCI6MjA2MjU2Mjk3MX0.bEqtAPxy-fB31FrsIh8Mn240udNrKWAsdv4akpjNg8Q'
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
+
 async function loadImages() {
   const { data, error } = await supabase.storage.from('mosaic').list('', {
     limit: 100, offset: 0, sortBy: { column: 'name', order: 'asc' }
