ce596b104c6ce43f6a182e57ffb90cca09f014d1
Steepee <114439934+Steppi1@users.noreply.github.com>
2025-05-15 00:22:18 +0200

    erg

--- theme.js
+++
@@ -1,7 +0,0 @@
-// theme.js
-export function initThemeToggle(buttonId = 'toggle-theme') {
-  const btn = document.getElementById(buttonId);
-  btn.addEventListener('click', () => {
-    document.body.classList.toggle('light-mode');
-  });
-}
