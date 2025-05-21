f02af0c36ee4e1ae33d0c08d13b80d9501858faa
Steepee <114439934+Steppi1@users.noreply.github.com>
2025-05-21 21:37:37 +0200

    test

--- files/nav.js
+++
@@ -1,5 +0,0 @@
-export function showArticleById(id) {
-  document.querySelectorAll('.article').forEach(a => a.classList.remove('open'));
-  const el = document.querySelector(`.article[data-id="${id}"]`);
-  if (el) el.classList.add('open');
-}
